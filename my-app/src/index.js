41fb47e986766954cba49ea4e61c6b69c76cdca5
steffanperera <steffanperera1997@gmail.com>
2025-11-12 10:45:57 +1100

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Counter from "./components/counter";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Counter />
   </React.StrictMode>
 );
