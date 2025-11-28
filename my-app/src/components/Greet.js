aaeeb92806d902b6b5cb4f72a85bd3b7aa08abfb
steffanperera <steffanperera1997@gmail.com>
2025-11-28 21:50:10 +1100

    update

--- my-app/src/components/Greet.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Greet = () => {
-  return <p>Hello! Greetings!</p>;
-};
-
-export default Greet;
--- vite-app/src/components/Information.jsx
+++
@@ -1,7 +0,0 @@
-export const Information = () => {
-  return (
-    <p className="read-the-docs">
-      Click on the Vite and React logos to learn more
-    </p>
-  );
-};
